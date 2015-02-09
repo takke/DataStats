a0adbf9523c6191289b46c5d4a272b9e367fca6b
takke <takke30@gmail.com>
2015-02-09 21:58:09 +0900

    appcompat free

--- app/src/main/java/jp/takke/tktrafficmonitorsample/MainActivity.java
+++ app/src/main/java/jp/takke/tktrafficmonitorsample/MainActivity.java
@@ -1,16 +1,16 @@
 package jp.takke.tktrafficmonitorsample;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
