10c0bc23741296210f6fe6ed63ba61cd45bdf14d
takke <takke30@gmail.com>
2015-03-20 08:26:53 +0900

    Implement interpolation

--- app/src/main/java/jp/takke/datastats/MySurfaceView.java
+++ app/src/main/java/jp/takke/datastats/MySurfaceView.java
@@ -61,8 +61,10 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     private Drawable downloadDrawable;
     
     // 補間モード
-    private boolean mInterpolateMode = false;
+    private boolean mInterpolateMode = true;
 
+    private int mLastPTx;
+    private int mLastPRx;
 
     public MySurfaceView(Context context) {
         super(context);
@@ -174,21 +176,24 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
         if (mTrafficList.size() >= 1) {
             final Traffic t = mTrafficList.getLast();
-
+            final long now = System.currentTimeMillis();
+            
             // upload gradient
             if (uploadDrawable == null) {
                 uploadDrawable = resources.getDrawable(R.drawable.upload_background);
             }
-            final int endUX = (int) (t.pTx / 1000f * xDownloadStart);
-            uploadDrawable.setBounds(0, 0, endUX, mScreenHeight);
+            final int pTx = mInterpolateMode ? interpolate(t, now, true) : t.pTx;
+            mLastPTx = pTx;
+            uploadDrawable.setBounds(0, 0, (int) (pTx / 1000f * xDownloadStart), mScreenHeight);
             uploadDrawable.draw(canvas);
             
             // download gradient
             if (downloadDrawable == null) {
                 downloadDrawable = resources.getDrawable(R.drawable.download_background);
             }
-            int endDX = (int) (xDownloadStart + t.pTx / 1000f * xDownloadStart);
-            downloadDrawable.setBounds(xDownloadStart, 0, endDX, mScreenHeight);
+            final int pRx = mInterpolateMode ? interpolate(t, now, false) : t.pRx;
+            mLastPRx = pRx;
+            downloadDrawable.setBounds(xDownloadStart, 0, (int) (xDownloadStart + pRx / 1000f * xDownloadStart), mScreenHeight);
             downloadDrawable.draw(canvas);
 
             // upload text
@@ -250,6 +255,80 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
 
         mSurfaceHolder.unlockCanvasAndPost(canvas);
+    }
+
+
+    private int interpolate(Traffic t, long now, boolean getTx) {
+
+        final int currentP = getTx ? t.pTx : t.pRx;
+        
+        final int n = mTrafficList.size() - 1;
+        if (n < 2) {
+            return currentP;
+        } else {
+
+            // 最後の2つ分の差分時間を補間に使う
+            final long lastIntervalTime = mTrafficList.get(n).time - mTrafficList.get(n-1).time;
+            final long elapsed = now - mTrafficList.get(n).time;
+            if (elapsed > lastIntervalTime * 3) {
+                // 十分時間が経過しているので収束させる
+                return currentP;
+            } else {
+                
+                // 補間準備
+                final double[] x = new double[n + 1];
+                final double[] y = new double[n + 1];
+                for (int i = 0; i < n + 1; i++) {
+                    final Traffic t1 = mTrafficList.get(i);
+                    x[i] = t1.time;
+                    y[i] = getTx ? t1.pTx : t1.pRx;
+                }
+
+                // 要は lastIntervalTime 分だけ遅れて描画される感じ
+                // でも lastIntervalTime だと遅すぎるので少し短くしておく
+                final long at = now - lastIntervalTime / 2;
+                final int p = (int) lagrange(n, x, y, at);
+                if (p < 0) {
+                    return 0;
+                } else if (p > 1000) {
+                    return 1000;
+                }
+                
+                // 前回の差分から方向を算出し、現在値を超えていたら超えないようにする
+                final int lastP = getTx ? mLastPTx : mLastPRx;
+                final int direction = p - lastP;
+                if (direction > 0 && p > currentP) {
+                    // 上昇方向で超過しているので制限する
+                    return currentP;
+                } else if (direction < 0 && p < currentP) {
+                    // 下降方向で下回っているので制限する
+                    return currentP;
+                }
+                
+                return p;
+            }
+        }
+    }
+
+
+    private double lagrange(int n, double[] x, double[] y, double x1) {
+
+        double s1, s2, y1 = 0.0;
+        int i1, i2;
+
+        for (i1 = 0; i1 <= n; i1++) {
+            s1 = 1.0;
+            s2 = 1.0;
+            for (i2 = 0; i2 <= n; i2++) {
+                if (i2 != i1) {
+                    s1 *= (x1 - x[i2]);
+                    s2 *= (x[i1] - x[i2]);
+                }
+            }
+            y1 += y[i1] * s1 / s2;
+        }
+
+        return y1;
     }
 
 
