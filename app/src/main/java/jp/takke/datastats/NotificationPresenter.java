1196b21916a854054dccc3b3b8714a19094b0df6
takke <hiroaki@v7.com>
2019-10-25 14:05:53 +0900

    Migrate to AndroidX

--- app/src/main/java/jp/takke/datastats/NotificationPresenter.java
+++ app/src/main/java/jp/takke/datastats/NotificationPresenter.java
@@ -7,9 +7,10 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.RemoteViews;
+
+import androidx.core.app.NotificationCompat;
 
 import java.lang.ref.WeakReference;
 
