7cdf0cbe644e9ce4a2cf08ea15bb1250eaefb8d7
takke <hiroaki@v7.com>
2019-11-06 14:50:44 +0900

    App.kt

---
+++ app/src/main/java/jp/takke/datastats/App.kt
@@ -0,0 +1,23 @@
+package jp.takke.datastats
+
+import android.app.Application
+
+import jp.takke.util.MyLog
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // ログの設定
+        MyLog.setContext(this)
+
+        MyLog.i("start")
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+
+        MyLog.close()
+    }
+}
