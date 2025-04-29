3cc0da1f1a87a07cc6282ff6328c58f3f21fbb41
takke <takke30@gmail.com>
2025-04-30 00:27:29 +0900

    MyTrafficUtil

--- app/src/main/java/jp/takke/datastats/MyTrafficUtil.kt
+++ app/src/main/java/jp/takke/datastats/MyTrafficUtil.kt
@@ -1,90 +1,75 @@
-package jp.takke.datastats;
+package jp.takke.datastats
 
-import android.content.res.Resources;
+import android.content.res.Resources
+import java.io.BufferedReader
+import java.io.FileReader
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+object MyTrafficUtil {
+  fun convertByteToKb(bytes: Long): Long {
+    return bytes / 1024
+  }
 
-public class MyTrafficUtil {
+
+  fun convertByteToD1Kb(bytes: Long): Long {
+    val b1023 = bytes % 1024 // [0, 1023]
+
+    // to [0, 9]
+    if (b1023 >= 900) return 9
+
+    if (b1023 == 0L) return 0
+
+    if (b1023 <= 100) return 1
+
+    return b1023 / 100
+  }
 
 
-    public static long convertByteToKb(long bytes) {
-        return bytes / 1024;
+  fun getTextShadowColorByBytes(resources: Resources, bytes: Long): Int {
+    if (bytes < Config.middleLimit) {
+      return resources.getColor(R.color.textShadowColorLow)
     }
-
-
-    public static long convertByteToD1Kb(long bytes) {
-
-        final long b1023 = bytes % 1024; // [0, 1023]
-
-        // to [0, 9]
-        if (b1023 >= 900)
-            return 9;
-
-        if (b1023 == 0)
-            return 0;
-
-        if (b1023 <= 100)
-            return 1;
-
-        return b1023 / 100;
+    if (bytes < Config.highLimit) {
+      return resources.getColor(R.color.textShadowColorMiddle)
     }
+    return resources.getColor(R.color.textShadowColorHigh)
+  }
 
 
-    static int getTextShadowColorByBytes(Resources resources, long bytes) {
-
-        if (bytes < Config.middleLimit) {
-            return resources.getColor(R.color.textShadowColorLow);
-        }
-        if (bytes < Config.highLimit) {
-            return resources.getColor(R.color.textShadowColorMiddle);
-        }
-        return resources.getColor(R.color.textShadowColorHigh);
+  fun getTextColorByBytes(resources: Resources, bytes: Long): Int {
+    if (bytes < Config.middleLimit) {
+      return resources.getColor(R.color.textColorLow)
     }
-
-
-    static int getTextColorByBytes(Resources resources, long bytes) {
-
-        if (bytes < Config.middleLimit) {
-            return resources.getColor(R.color.textColorLow);
-        }
-        if (bytes < Config.highLimit) {
-            return resources.getColor(R.color.textColorMiddle);
-        }
-        return resources.getColor(R.color.textColorHigh);
+    if (bytes < Config.highLimit) {
+      return resources.getColor(R.color.textColorMiddle)
     }
+    return resources.getColor(R.color.textColorHigh)
+  }
 
 
-    static long getLoopbackRxBytes() {
+  val loopbackRxBytes: Long
+    get() = readLongValueFromFile("/sys/class/net/lo/statistics/rx_bytes")
 
-        return readLongValueFromFile("/sys/class/net/lo/statistics/rx_bytes");
+
+  val loopbackTxBytes: Long
+    get() = readLongValueFromFile("/sys/class/net/lo/statistics/tx_bytes")
+
+
+  private fun readLongValueFromFile(path: String): Long {
+    try {
+      val `in` = FileReader(path)
+      val br = BufferedReader(`in`)
+
+      val line = br.readLine()
+
+      br.close()
+      `in`.close()
+
+      if (line == null) {
+        return 0
+      }
+      return line.toLong()
+    } catch (ignored: Throwable) {
+      return 0
     }
-
-
-    static long getLoopbackTxBytes() {
-
-        return readLongValueFromFile("/sys/class/net/lo/statistics/tx_bytes");
-    }
-
-
-    private static long readLongValueFromFile(String path) {
-
-        try {
-            final FileReader in = new FileReader(path);
-            final BufferedReader br = new BufferedReader(in);
-
-            final String line = br.readLine();
-
-            br.close();
-            in.close();
-
-            if (line == null) {
-                return 0;
-            }
-            return Long.valueOf(line);
-
-        } catch (Throwable ignored) {
-            return 0;
-        }
-    }
+  }
 }
